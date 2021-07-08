0796dbf1b499104630c8ae1473af06abdb3745ba
Outdam Meas <mstdam@gmail.com>
2021-07-08 22:40:08 +0800

    [1] initialize spring project

---
+++ src/main/java/com/demo/sbtrip/SbtripApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sbtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbtripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/sbtrip/SbtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.sbtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
